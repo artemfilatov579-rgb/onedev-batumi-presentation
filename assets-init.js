93f6011294d94b2b12876649c21fb04f99d23994
artemfilatov579-rgb <artemfilatov579@gmail.com>
2026-07-27 12:50:40 +0300

    Ramada Residences: разворот с фото застройщика (каркас + лоадер)

---
+++ assets-init.js
@@ -0,0 +1,8 @@
+(function(){var M=window.__IMG||{};
+function fix(s){return s.replace(/__IMGREF_(I\d+)__/g,function(_,k){return M[k]||''})}
+document.querySelectorAll('img[data-src]').forEach(function(e){
+  e.src=fix(e.getAttribute('data-src'));e.removeAttribute('data-src');});
+document.querySelectorAll('[data-bg]').forEach(function(e){
+  e.style.backgroundImage="url('"+fix(e.getAttribute('data-bg'))+"')";
+  e.removeAttribute('data-bg');});
+})();
